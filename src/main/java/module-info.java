59ce8d78797a124a92e524115e98b1cb71bfea60
gnsals0904 <gnsals0904@naver.com>
2025-03-04 10:11:40 +0900

    [Feat] 프로젝트 스켈레톤 코드 추가

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.nhnacademy {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires transitive javafx.graphics;
+
+    opens com.nhnacademy to javafx.fxml;
+
+    exports com.nhnacademy;
+}
