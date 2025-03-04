e3640a9edcdd018477e15cb7a16f39094f14c3c6
flashdragon <yooh3574@naver.com>
2025-03-04 16:02:09 +0900

    폭탄 벽돌 수정

--- yongjae/src/main/java/com/nhnacademy/BombBrick.java
+++ yongjae/src/main/java/com/nhnacademy/BombBrick.java
@@ -10,28 +10,4 @@ public class BombBrick extends Brick {
         this.color = Color.GRAY;
         this.HP = 1;
     }
-
-    // 공과 충돌 여부 확인
-    @Override
-    public boolean checkCollision(Ball ball) {
-        if (isDestroyed) {
-            return false; // 이미 파괴된 벽돌은 충돌하지 않음
-        }
-
-        double ballX = ball.getX();
-        double ballY = ball.getY();
-        double ballRadius = ball.getRadius();
-
-        // 공이 벽돌의 경계와 충돌했는지 확인
-        boolean collision = ballX + ballRadius > x &&
-                ballX - ballRadius < x + width &&
-                ballY + ballRadius > y &&
-                ballY - ballRadius < y + height;
-
-        if (collision) {
-            isDestroyed = true;
-        }
-
-        return collision;
-    }
 }
