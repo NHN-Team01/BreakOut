d096c813ec731aee1d9225f1f886798a15b7d3b1
NaIlHwan <nlhjobs01@naver.com>
2025-03-05 13:30:53 +0900

    [Refactor] Paddle 클래스 상속 관계 추가, 중복 멤버 및 메서드 삭제

--- ilhwan/src/main/java/com/nhnacademy/Paddle.java
+++ ilhwan/src/main/java/com/nhnacademy/Paddle.java
@@ -3,22 +3,13 @@ package com.nhnacademy;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
-public class Paddle {
-    private double x; // 패들의 중심 x 좌표
-    private double y; // 패들의 중심 y 좌표
-    private double width; // 패들의 너비
-    private double height; // 패들의 높이
+public class Paddle extends Rectangle {
     private double speed; // 패들의 이동 속도
-    private Color color; // 패들의 색상
 
     // 생성자
     public Paddle(double x, double y, double width, double height, double speed, Color color) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+        super(x, y, width, height, color);
         this.speed = speed;
-        this.color = color;
     }
 
     // 패들을 그리는 메서드
