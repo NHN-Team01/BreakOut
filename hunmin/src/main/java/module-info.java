6ab1c19c78c2143d538a752d714c1ec570bee2f9
gnsals0904 <gnsals0904@naver.com>
2025-03-07 16:17:47 +0900

    [Refactor] 사용하는 module 정보 수정

--- hunmin/src/main/java/module-info.java
+++ hunmin/src/main/java/module-info.java
@@ -11,4 +11,10 @@ module com.nhnacademy {
     opens com.nhnacademy.legacy to javafx.fxml;
     exports com.nhnacademy.controller;
     opens com.nhnacademy.controller to javafx.fxml;
+    exports com.nhnacademy.domain.data;
+    opens com.nhnacademy.domain.data to javafx.fxml;
+    exports com.nhnacademy.domain.model;
+    opens com.nhnacademy.domain.model to javafx.fxml;
+    exports com.nhnacademy.view to javafx.fxml;
+    opens com.nhnacademy.view to javafx.fxml;
 }
